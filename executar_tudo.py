d0aa3e8587cd5fba3a4365b3b999f52ffa5cb1c5
Wedson Tavares <wedsonsobral@gmail.com>
2025-03-29 18:41:26 -0300

    env e gitignore

--- executar_tudo.py
+++ executar_tudo.py
@@ -6,7 +6,7 @@ caminho_base = os.path.dirname(os.path.abspath(__file__))
 
 # Caminhos dos scripts
 script_analisar_operadoras = os.path.join(caminho_base, 'analisar_operadoras.py')
-script_teste3 = os.path.join(caminho_base, 'teste3.py')
+script_teste3 = os.path.join(caminho_base, 'demonstracoes_contabeis.py')
 
 # Executar o script de análise das operadoras
 print("Executando o script 'analisar_operadoras.py'...")
@@ -16,10 +16,10 @@ try:
 except subprocess.CalledProcessError as e:
     print(f"Erro ao executar 'analisar_operadoras.py': {e}")
 
-# Executar o script principal (teste3.py)
-print("\nExecutando o script 'teste3.py'...")
+# Executar o script principal (demonstracoes_contabeis.py)
+print("\nExecutando o script 'demonstracoes_contabeis.py'...")
 try:
     subprocess.run(['python', script_teste3], check=True)
-    print("\nScript 'teste3.py' executado com sucesso!")
+    print("\nScript 'demonstracoes_contabeis.py' executado com sucesso!")
 except subprocess.CalledProcessError as e:
-    print(f"Erro ao executar 'teste3.py': {e}")
+    print(f"Erro ao executar 'demonstracoes_contabeis.py': {e}")
